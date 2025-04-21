f54255c51f4fdb7a36bfb2b2a6f0920f140d5897
srob <sjroberts@gmail.com>
2025-04-21 19:25:39 +0100

    Initial Commit

---
+++ PlanningPoker/PlanningPokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanningPokerApp.swift
+//  PlanningPoker
+//
+//  Created by Simon Roberts on 21/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlanningPokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlanningPokerTests/PlanningPokerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlanningPokerTests.swift
+//  PlanningPokerTests
+//
+//  Created by Simon Roberts on 21/04/2025.
+//
+
+import Testing
+@testable import PlanningPoker
+
+struct PlanningPokerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
